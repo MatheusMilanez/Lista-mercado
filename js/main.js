d9a47c60212af7a7b133fe86425dd4aa385cbb07
Matheus Milanez <matheusmmlopes19@gmail.com>
2023-02-09 16:49:13 -0300

    Cria um 'li' na lista com quantidade e nome do produt

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 const form = document.getElementById("adicionar-produto-quantidade")
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (Evento) => {
     Evento.preventDefault() //Esse código interrope o evento padrão do formulário
     console.log("Funcionou")
-    console.log(Evento.target.elements['produto'].value)
-    console.log(Evento.target.elements['quantidade'].value)
+                            
+    criaElemento(Evento.target.elements["produto"].value ,Evento.target.elements["quantidade"].value)
+
+
 })
+
+function criaElemento(produto,quantidade){
+    //<li class="item"><strong>7</strong>Camisas</li>
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
+    
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += produto
+
+    lista.appendChild(novoItem)
+
+    console.log(novoItem)
+}
 
